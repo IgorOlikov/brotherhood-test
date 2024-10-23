7feaafbc43069592f744038783927d592ed455f8
IgorOlikov <i.olikov@mail.ru>
2024-10-23 18:12:01 +0500

    updated Employee Dto

--- app/src/Controller/EmployeeController.php
+++ app/src/Controller/EmployeeController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use App\DTO\EmployeeDto;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/api/v1')]
 class EmployeeController extends AbstractController
 {
     #[Route('/employee', name: 'app_employee')]
@@ -15,5 +18,16 @@ class EmployeeController extends AbstractController
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/EmployeeController.php',
         ]);
+    }
+
+    #[Route('/employee/create', name: 'app_employee_create', methods: ['POST'])]
+    public function create(
+        #[MapRequestPayload(
+            acceptFormat: 'json',
+            validationGroups: ['create']
+        )] EmployeeDto $employeeDto
+    )
+    {
+        dd($employeeDto);
     }
 }
--- app/src/DTO/EmployeeDto.php
+++ app/src/DTO/EmployeeDto.php
@@ -8,7 +8,7 @@ use DateTimeImmutable;
 use Doctrine\DBAL\Types\Types;
 use Symfony\Component\Validator\Constraints as Assert;
 
-class EmployeeDto
+final class EmployeeDto
 {
     #[Assert\Type(type: Types::INTEGER, groups: ['update', 'patch'])]
     #[Assert\NotBlank(groups: ['update', 'patch'])]
@@ -18,24 +18,24 @@ class EmployeeDto
         message: 'Employee with ID {{ value }} does not exist.',
         groups: ['update', 'patch']
     )]
-    private ?int $id = null;
+    public ?int $id = null;
 
     #[Assert\Type(type: Types::STRING, groups: ['update', 'patch'])]
     #[Assert\Choice(choices: ['working', 'dismissal'], groups: ['update', 'patch'], match: true)]
     #[Assert\NotBlank(groups: ['update', 'patch'])]
-    #[Assert\Length(min: 3, max: 20)]
-    private ?string $status = null;
+    #[Assert\Length(min: 3, max: 20, groups: ['update', 'patch'])]
+    public ?string $status = null;
 
     #[Assert\Type(type: Types::STRING, groups: ['create', 'update'])]
-    #[Assert\NotBlank(groups: ['update', 'patch'])]
+    #[Assert\NotBlank(groups: ['create', 'update'])]
     #[EntityUniqueField(
         entityClass: 'App\Entity\Employee',
         field: 'fullName',
         message: 'Employee with {{ field }} {{ value }} already exists.',
         groups: ['create', 'update']
     )]
-    #[Assert\Length(min: 6, max: 30)]
-    private ?string $fullName = null;
+    #[Assert\Length(min: 6, max: 30, groups: ['create', 'update'])]
+    public ?string $fullName = null;
 
     #[Assert\Type(type: Types::STRING, groups: ['create', 'update'])]
     #[Assert\NotBlank(groups: ['create', 'update'])]
@@ -44,8 +44,8 @@ class EmployeeDto
         groups: ['create', 'update'],
         match: true
     )]
-    #[Assert\Length(min: 4, max: 25)]
-    private ?string $position = null;
+    #[Assert\Length(min: 4, max: 25, groups: ['create', 'update'])]
+    public ?string $position = null;
 
     #[Assert\Type(type: Types::STRING, groups: ['create', 'update'])]
     #[Assert\NotBlank(groups: ['create', 'update'])]
@@ -56,17 +56,27 @@ class EmployeeDto
         message: 'Employee with {{ field }} {{ value }} already exists.',
         groups: ['create', 'update']
     )]
-    #[Assert\Length(min: 6, max: 20)]
-    private ?string $email = null;
+    #[Assert\Length(min: 6, max: 20, groups: ['create', 'update'])]
+    public ?string $email = null;
 
     #[Assert\Type(type: Types::STRING, groups: ['create', 'update'])]
     #[Assert\NotBlank(groups: ['create', 'update'])]
-    #[Assert\Length(min: 6, max: 12)]
-    private ?string $phoneNumber = null;
+    #[Assert\Length(min: 6, max: 12, groups: ['create', 'update'])]
+    public ?string $phoneNumber = null;
 
-    #[Assert\Date(groups: ['create', 'update'])]
+    #[Assert\DateTime('Y-m-d')]
+    #[Assert\NotNull(groups: ['create', 'update'])]
     #[Assert\NotBlank(groups: ['create', 'update'])]
-    private ?DateTimeImmutable $dateOfBrith = null;
+    #[Assert\LessThan('today', groups: ['create', 'update'])]
+    #[Assert\GreaterThan(value: '1950-01-01', groups: ['create', 'update'])]
+    public ?DateTimeImmutable $dateOfBrith = null;
+
+    //#[Assert\NotNull(groups: ['create', 'update'])]
+    //#[Assert\Date(groups: ['create', 'update'])]
+    //#[Assert\NotBlank(groups: ['create', 'update'])]
+    //#[Assert\LessThan(value: new DateTimeImmutable(), groups: ['create', 'update'])]
+    //#[Assert\GreaterThan(value: '1950-01-01', groups: ['create', 'update'])]
+    //public ?string $dateOfBrith = null;
 
 
 }
