41342cbaa3c608704f66d10c333e466b1be1c3f9
IgorOlikov <i.olikov@mail.ru>
2024-10-29 16:48:51 +0500

    added readme TODO

--- app/src/Controller/AuthController.php
+++ app/src/Controller/AuthController.php
@@ -6,14 +6,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/api/v1/auth')]
 class AuthController extends AbstractController
 {
     #[Route('/auth', name: 'app_auth')]
-    public function index(): JsonResponse
+    public function register(): JsonResponse
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/AuthController.php',
         ]);
     }
+
+
+
 }
