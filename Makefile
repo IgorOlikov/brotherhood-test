4f5fc122fc5430726ef518b66707b55dc4cdfde1
IgorOlikov <i.olikov@mail.ru>
2024-11-15 11:41:02 +0500

    added fixture bundles for tests

--- Makefile
+++ Makefile
@@ -1,2 +1,8 @@
 test:
 	docker compose run --rm cli php bin/phpunit
+
+test-migration:
+	docker compose run --rm cli bin/console doctrine:migrations:migrate --env=test
+
+test-fixtures:
+	docker compose run --rm cli bin/console doctrine:fixtures:load --env=test
--- app/tests/Functional/EmployeeControllerTest.php
+++ app/tests/Functional/EmployeeControllerTest.php
@@ -5,10 +5,17 @@ namespace App\Tests\Functional;
 
 use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
 
+
 class EmployeeControllerTest extends ApiTestCase
 {
+
+
     public function testSomething(): void
     {
+
+
+
+
         $response = static::createClient()->request('GET', '/api/v1/employee/{slug}');
 
 
