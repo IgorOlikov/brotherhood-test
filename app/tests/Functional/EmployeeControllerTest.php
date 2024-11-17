07ca0e3bada9f87b1fd6493c40207c718ee9f6e4
IgorOlikov <i.olikov@mail.ru>
2024-11-17 18:25:02 +0500

    added employee controller show test

--- app/tests/Functional/EmployeeControllerTest.php
+++ app/tests/Functional/EmployeeControllerTest.php
@@ -3,11 +3,17 @@
 namespace App\Tests\Functional;
 
 
-use App\DataFixtures\AppFixtures;
+use App\DataFixtures\EmployeeFixtures;
+use App\Entity\Employee;
+use Doctrine\Common\DataFixtures\Executor\AbstractExecutor;
 use Liip\TestFixturesBundle\Services\DatabaseToolCollection;
 use Liip\TestFixturesBundle\Services\DatabaseTools\AbstractDatabaseTool;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\Normalizer\AbstractObjectNormalizer;
+use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
+use Symfony\Component\Serializer\SerializerInterface;
 
 
 class EmployeeControllerTest extends WebTestCase
@@ -18,6 +24,18 @@ class EmployeeControllerTest extends WebTestCase
 
     /** @var KernelBrowser */
     private KernelBrowser $client;
+    private AbstractExecutor $executor;
+
+    /** @var SerializerInterface $serializer  */
+    private SerializerInterface $serializer;
+
+    private array $serializerContext = [
+        DateTimeNormalizer::FORMAT_KEY => 'Y-m-d',
+        AbstractNormalizer::GROUPS => ['public'],
+        AbstractObjectNormalizer::SKIP_UNINITIALIZED_VALUES => false,
+        AbstractObjectNormalizer::SKIP_NULL_VALUES => false
+    ];
+
 
     protected function setUp(): void
     {
@@ -26,26 +44,42 @@ class EmployeeControllerTest extends WebTestCase
         $this->client = static::createClient();
 
         $this->databaseTool = self::getContainer()->get(DatabaseToolCollection::class)->get();
+
+        $this->executor = $this->databaseTool->loadFixtures([EmployeeFixtures::class]);
+
+        $this->serializer = self::getContainer()->get(SerializerInterface::class);
     }
 
     public function testEmployeeIndex(): void
     {
-        $executor = $this->databaseTool->loadFixtures([AppFixtures::class]);
-
-        ///** @var Project $project */
-        //$project = $executor->getReferenceRepository()->getReference('project', Project::class);
-
-
-        $this->client->request('GET', '/api/v1/project');
+        $this->client->request('GET', '/api/v1/employee');
 
         $this->assertResponseIsSuccessful();
     }
 
+
     public function testEmployeeShow()
     {
+        /** @var Employee $employee */
+        $employee = $this->executor->getReferenceRepository()->getReference('employee', Employee::class);
+
+        $this->client->request('GET', "/api/v1/employee/{$employee->getSlug()}");
+
+        $this->assertResponseIsSuccessful();
+
+        $jsonResponseContent = $this->client->getResponse()->getContent();
+
+        $this->assertJson($jsonResponseContent);
+
+        $this->assertJsonStringEqualsJsonString($this->serializer->serialize(
+            data: $employee,
+            format: 'json',
+            context: $this->serializerContext
+        ),
+            $jsonResponseContent);
 
     }
-
+    /**
     public function testEmployeeStore()
     {
 
@@ -69,7 +103,7 @@ class EmployeeControllerTest extends WebTestCase
     {
 
     }
-
+    */
 
 
 
